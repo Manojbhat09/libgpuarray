041e9e0c282a6b555782f30255afb30f72204673
Manoj <manojbhat09@gmail.com>
2020-05-03 11:27:21 +0000

    Changing setup

--- setup.py
+++ setup.py
@@ -130,5 +130,5 @@ setup(name='pygpu',
                               'blas_api.h', 'numpy_compat.h',
                               'collectives.h', 'collectives_api.h']},
       ext_modules=cythonize(exts),
-      install_requires=['mako>=0.7', 'six'],
+      install_requires=['mako==0.7', 'six'],
       )
